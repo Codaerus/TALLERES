65bb1aec24cde67ee25a5e275da16e7752dfffb1
Codaerus <christianqc91@gmail.com>
2025-06-26 20:51:11 -0500

    Update doc.h

--- POO_SNAKE/doc.h
+++ POO_SNAKE/doc.h
@@ -15,4 +15,18 @@ SetConsoleCursorPosition
 Descripción: Función que mueve el cursor de la consola a la posición dada por una estructura COORD.
 */
 BOOL SetConsoleCursorPosition(HANDLE hConsoleOutput, COORD dwCursorPosition);
-
+/*
+srand
+Descripción: Inicializa la semilla para la generación de números pseudoaleatorios con rand().
+*/
+void srand(unsigned int seed);
+/*
+rand
+Descripción: Genera un número pseudoaleatorio entero entre 0 y RAND_MAX
+*/
+int rand(void);
+/*
+cin.get
+Descripción: Lee un carácter desde la entrada estándar (normalmente el teclado). Se usa comúnmente para esperar una entrada del usuario, como pulsar Enter.
+*/
+int std::istream::get();
